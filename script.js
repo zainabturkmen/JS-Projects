0b99727c9389104506a71f044380432577dc0e4e
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-07 16:33:19 +0430

    init

---
+++ script.js
@@ -0,0 +1,29 @@
+// javascript
+
+// variables
+
+// reassigning
+
+// let name, age, feild;
+
+// name = "Zainab";
+// console.log(name)
+
+// name = "bib"
+// age = "21"
+// feild = "CS"
+
+// console.log(name)
+// console.log(age)
+// console.log(feild)
+
+let num = 'hi ';
+let num1 = 2;
+console.log(num + num1)
+
+const myFriends = ['gul', 'honey', 'bob']
+
+let frinds = myFriends[2]
+console.log(frinds)
+
+console.log(myFriends[1])
