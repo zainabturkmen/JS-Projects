277021dfa9e0b769e04e908c66dff425908c0c03
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-08 11:31:06 +0430

    EXAMPLES

--- script.js
+++ script.js
@@ -3,15 +3,12 @@
 const wallHeight = 80;
 
 function calculate(value){
-    console.log(`Hey I am calculating in cm ${value * 2.54} cm`)
+    const myValue = value * 2.54
+    return myValue
 }
-
-
 
 const width = calculate(100);
 const height = calculate(wallHeight);
 
 const result = [width, height]
-
-// console.log(height)
-// console.log(width)
+console.log(result);
