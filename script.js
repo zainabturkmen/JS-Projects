34251b7380c5c20edb2827e40e98f5a0c8dfe48b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-08 14:53:18 +0430

    EXAMPLES

--- script.js
+++ script.js
@@ -3,10 +3,24 @@
 const num1 = 2;
 const num2 = 6;
 
-if(num1 < num2){
-    console.log("hey he")
-}else{
-    console.log("hooo");
+// if(num1 < num2){
+//     console.log("hey he")
+// }else{
+//     console.log("hooo");
+// }
+
+switch(num1){
+    case 1:
+        console.log('yammy 1')
+        break;
+    case 2:
+        console.log('yammy 2')
+        break;
+    case 3:
+        console.log('yammy 3')
+        break;
+        default:
+            console.log("fail fail fail")
 }
 
 
