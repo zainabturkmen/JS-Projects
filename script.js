1310a5059ffd684c197579d37012263b53e77437
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-08 10:56:13 +0430

    init

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 
-function myFunction(name, age){
-    console.log(`hey there i am ${name} am ${age} old`)
-    // return name+ age
-}
 
-myFunction("Zainab", "21")
-myFunction("bo", "23")
-myFunction("Znab", "21")
-
-// console.log(myFunction("Zainab", "21"));
