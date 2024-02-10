ab0bb150209e3c2ae84e34911ce18ecc59b73ef9
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-10 07:37:43 +0430

    EXAMPLES

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const valu = 2 === 3;
+
+valu ?   console.log("its ture"):   console.log("itsfals");
+
+// if(valu){
+//     console.log("its ture")
+// }else{
+//     console.log("its false");
+// }
