79fde3b3d0fcbc24ae6c85d6dc8232ce2e62b880
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-10 09:08:21 +0430

    EXAMPLES

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ valu ?   console.log("its ture"):   console.log("itsfals");
 //     console.log("its ture")
 // }else{
 //     console.log("its false");
-// }
+
+
+// forEach method iteraate over the arrays
+
+const myArrays = ["johne", 6, "nice", true]
+
+myArrays.forEach(function(item){
+    console.log(item);
+})
