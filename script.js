dd605db8131074e5be2063c8877f7c9992ec2e4d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-12 08:54:56 +0430

    EXAMPLES

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 const h1 = document.querySelector('h1')      
                   
 h1.style.color = 'red';    
+
+const bodyDiv = document.createElement('div')
+
+const text = document.createTextNode('hey i am zainab')
+
+bodyDiv.append(text)
+
+document.body.appendChild(bodyDiv)
