d75eaf9ad68c45f53e1da8e761181bee3218080e
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-17 21:21:06 +0430

    Project-two

--- Project-two/app.js
+++ Project-two/app.js
@@ -2,8 +2,13 @@
 
 const sidebarToggle = document.querySelector(".sidebar-toggle");
 const sidebar = document.querySelector(".sidebar");
-const closeBtn = document.querySelector("close-btn");
+const closeBtn = document.querySelector(".close-btn");
 
 sidebarToggle.addEventListener("click", function(){
     sidebar.classList.toggle("show-sidebar")
+})
+
+closeBtn.addEventListener("click", function(){
+    // sidebar.classList.remove("show-sidebar");
+    sidebar.classList.toggle("show-sidebar");
 })
