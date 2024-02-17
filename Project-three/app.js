7ff209f7ad43dfe8930419179eae67034756cd95
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-17 21:45:36 +0430

    Project-three

--- Project-three/app.js
+++ Project-three/app.js
@@ -2,3 +2,4 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
