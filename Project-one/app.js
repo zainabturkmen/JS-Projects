b1ae1b7e286027d58b9a26f2128ff43973e25915
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-17 20:01:04 +0430

    Project one

--- Project-one/app.js
+++ Project-one/app.js
@@ -5,7 +5,7 @@
 // toggle - toggles class
 
 const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector('.links')
+const links = document.querySelector(".links")
 
 navToggle.addEventListener("click", function(){
     links.classList.toggle("show-links")
