733fe800db4c891faf53cc1c2703e2ba5aa4ff5b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-17 21:52:09 +0430

    Project-three

--- Project-three/app.js
+++ Project-three/app.js
@@ -7,3 +7,10 @@ const modalBtn = document.querySelector(".modal-btn");
 const closeBtn = document.querySelector(".close-btn");
 const modalOverlay = document.querySelector(".modal-overlay");
 
+modalBtn.addEventListener('click', function(){
+
+})
+
+closeBtn.addEventListener('click', function(){
+
+})
