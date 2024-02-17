c96a7cc4bc6d6511d1899e13726e24018bbf9c04
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-17 21:59:05 +0430

    Project-three

--- Project-three/app.js
+++ Project-three/app.js
@@ -9,7 +9,7 @@ const modalOverlay = document.querySelector(".modal-overlay");
 
 modalBtn.addEventListener('click', function(){
     modalOverlay.classList.toggle("open-modal")
-})
+});
 
 closeBtn.addEventListener('click', function(){
     modalOverlay.classList.remove("open-modal")
