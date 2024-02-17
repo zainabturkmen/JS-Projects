0f612797455ddb2321f9432c1e2274cf4ff224f7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-17 21:48:00 +0430

    Project-three

--- Project-three/app.js
+++ Project-three/app.js
@@ -4,4 +4,5 @@
 // when user clicks close-btn remove .open-modal from modal-overlay
 
 const modalBtn = document.querySelector(".modal-btn");
-const closeBtn = document.querySelector(".close-btn")
+const closeBtn = document.querySelector(".close-btn");
+const 
