e0989f12f991d8d77e31ec4a94d21205e5867739
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-20 15:54:24 +0430

    SWIPPER

---
+++ project-six/index.js
@@ -0,0 +1,21 @@
+
+// 
+// <!-- Initialize Swiper -->
+import {Swiper} from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
