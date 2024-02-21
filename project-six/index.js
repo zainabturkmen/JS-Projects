1f1f293201a60f62ee77ad3ee581caf06bd76ef3
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-21 18:13:28 +0430

    SWIPPER

--- project-six/index.js
+++ project-six/index.js
@@ -1,21 +1,31 @@
 
-// 
-// <!-- Initialize Swiper -->
-import Swiper from "./swipper.js"
+
+import Swiper from "../node_modules/swiper/swiper-bundle.min.mjs"
 
 var swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+        delay: 2500,
+        disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  });
+});
