a2a509e08c3300b6268076d2863239bc1cdde803
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-22 19:37:23 +0430

    adding some changes

--- project-eight/app.js
+++ project-eight/app.js
@@ -26,5 +26,16 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date();
+let futureDate = new Date(2024, 2, 22, 19, 3, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+// const secs = futureDate.getSeconds();
+
+let month = futureDate.getMonth()
+month = months[month];
+const date = futureDate.getDate()
+const weekday = weekdays[futureDate.getDay()]
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}: ${mins}am`
 
