c61545e679d66a914737f81fb1d22e24c6b9db9f
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-22 16:04:26 +0430

    adding some changes

--- projecy-seven/app.js
+++ projecy-seven/app.js
@@ -10,6 +10,12 @@
             btn.classList.remove("active");
             e.target.classList.add("active")
         });
-        
-    }
+        // hide other articles
+        articles.forEach(function(article){
+            article.classList.remove("active");
+        });
+
+        const element = document.getElementById(id)
+        element.classList.add("active")
+    };
  })
