9691e1dfe1ede810d4659ba1976cad43da62b603
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-22 16:00:44 +0430

    adding some changes

--- projecy-seven/app.js
+++ projecy-seven/app.js
@@ -1,7 +1,15 @@
- const btn = document.querySelectorAll(".btn-container");
+ const btns = document.querySelectorAll(".btn-container");
  const about = document.querySelector(".about");
  const articles = document.querySelectorAll(".content");
 
  about.addEventListener("click", function(e){
-
+    const id = e.target.dataset.id;
+    if(id){
+        // remove active from other or all buttons 
+        btns.forEach(function(btn){
+            btn.classList.remove("active");
+            e.target.classList.add("active")
+        });
+        
+    }
  })
