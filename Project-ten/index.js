f72c5e3be999ddf5da2b0ba000388b4798a60a0c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-29 19:00:12 +0430

    adding some changes

--- Project-ten/index.js
+++ Project-ten/index.js
@@ -1,1 +1,10 @@
 // js
+
+const People = [
+    {name: "Zainab", lastmane:"Turkmen", age:"21"},
+    {name: "joe", lastmane:"anderson", age:"22"},
+    {name: "su", lastmane:"susi", age:"23"},
+]
+
+const container
+
