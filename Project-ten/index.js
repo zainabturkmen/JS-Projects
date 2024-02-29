bb9bb4f2bbf908abcfec61e23d136122c5a33d56
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-29 19:10:21 +0430

    adding some changes

--- Project-ten/index.js
+++ Project-ten/index.js
@@ -4,7 +4,7 @@ const People = [
     {name: "Zainab", lastmane:"Turkmen", age:"21"},
     {name: "joe", lastmane:"anderson", age:"22"},
     {name: "su", lastmane:"susi", age:"23"},
-]
+];
 
 const container = document.querySelector("body");
 
