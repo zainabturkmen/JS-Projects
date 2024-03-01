efcc64eb73740d21520a68be9edb9f4f39efc59a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-01 07:50:29 +0430

    adding some changes

--- Project-ten/utils/getElement.js
+++ Project-ten/utils/getElement.js
@@ -1,6 +1,8 @@
 function getElement(selection){
     const element = document.querySelector(selection);
     if(element){
-        
+        return element
+    }else{
+        throw Error("you did not select element")
     }
 }
