dee1c9eec156ba3bb2089548cf7d70bcabfec71c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-01 09:32:26 +0430

    adding some changes

--- Project-ten/index.js
+++ Project-ten/index.js
@@ -7,9 +7,16 @@ import get from "./utils/getElement.js"
 const container = get(".container");
 const btn = get(".btn");
 const h2 = get(".h2");
+const h3 = get(".h3");
+const h4 = get(".h4");
 
 btn.addEventListener("click", () => {
     container.innerHTML = clickMe(people);
+    setTimeout(()=>{
+        h2.style.color= "blue"
+    }, 1000)
 });
+
+
 
 
