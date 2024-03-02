f6ebc8155d3c5773f68817a2a485eee9bfd719a7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-03-02 07:38:24 +0430

    adding some changes

--- Project-ten/index.js
+++ Project-ten/index.js
@@ -11,28 +11,29 @@ const h3 = get(".h3");
 const h4 = get(".h4");
 
 btn.addEventListener("click", () => {
-    // container.innerHTML = clickMe(people);
-    // callback hell
-    setTimeout(()=>{
-        h2.style.color= "blue";
-        setTimeout(() => {
-            h2.style.backgroundColor = "black"
-        }, 1500);
-        setTimeout(()=>{
-            h3.style.color = "red"
-            setTimeout(()=>{
-                h3.style.backgroundColor = "black"
-            },2500)
-            setTimeout(()=>{
-                h4.style.color = "green"
-                setTimeout(() => {
-                    h4.style.backgroundColor = "black"
-                }, 3500);
-            },3000)
-        },2000)
-    }, 1000);
+    addColor(100,h2, "red")
 });
 
 
 
+// container.innerHTML = clickMe(people);
+// callback hell
+ // setTimeout(()=>{
+    //     h2.style.color= "blue";
+    //     setTimeout(() => {
+    //         h2.style.backgroundColor = "black"
+    //     }, 1500);
+    //     setTimeout(()=>{
+    //         h3.style.color = "red"
+    //         setTimeout(()=>{
+    //             h3.style.backgroundColor = "black"
+    //         },2500)
+    //         setTimeout(()=>{
+    //             h4.style.color = "green"
+    //             setTimeout(() => {
+    //                 h4.style.backgroundColor = "black"
+    //             }, 3500);
+    //         },3000)
+    //     },2000)
+    // }, 1000);
 
